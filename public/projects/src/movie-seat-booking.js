b0508e6e89b8a4d4fdd657c4b5848f11e3e0e92d
opeidris <opeyemi@opeidris.com>
2020-03-17 22:05:59 +0800

    finished movie booking

--- public/projects/src/movie-seat-booking.js
+++ public/projects/src/movie-seat-booking.js
@@ -3,23 +3,60 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-let counter = 0;
 
-const ticketPrice = +movieSelect.value;
+//get data fromm local storage and populate the UI
+populateUI();
+let ticketPrice = +movieSelect.value;
+
+//Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+	localStorage.setItem('selectedMovieIndex', movieIndex);
+	localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 function updateSelectedCount() {
 	const selectedSeats = document.querySelectorAll('.row .seat.selected');
-	console.log(selectedSeats.length);
-	count.innerText = selectedSeats.length;
+
+	const seatsIndex = [ ...selectedSeats ].map((seat) => [ ...seats ].indexOf(seat));
+	console.log(seatsIndex);
+
+	localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+	selectedSeatsCount = selectedSeats.length;
+	count.innerText = selectedSeatsCount;
+	total.innerText = selectedSeatsCount * ticketPrice;
 }
 
-function calculatePrice(counter) {
-	price = ticketPrice * c;
-}
+function populateUI() {
+	const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
 
+	if (selectedSeats !== null && selectedSeats.length > 0) {
+		seats.forEach((seat, index) => {
+			if (selectedSeats.indexOf(index) > -1) {
+				seat.classList.add('selected');
+			}
+		});
+	}
+
+	const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+	if (selectedMovieIndex !== null) {
+		movieSelect.selectedIndex = selectedMovieIndex;
+	}
+}
+// Movie select Event
+
+movieSelect.addEventListener('change', (e) => {
+	ticketPrice = +e.target.value;
+	setMovieData(e.target.selectedIndex, e.target.value);
+	updateSelectedCount();
+});
+//Seat Click event
 container.addEventListener('click', (e) => {
 	if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
 		e.target.classList.toggle('selected');
 		updateSelectedCount();
 	}
 });
+
+updateSelectedCount();
