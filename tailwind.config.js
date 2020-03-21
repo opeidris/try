c28ac8951cfa30bd13741b52df772a7ffa855825
opeidris <opeyemi@opeidris.com>
2020-03-21 15:51:14 +0800

    html and css for exchange rate calculator

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 module.exports = {
 	theme: {
+		backgroundSize: {
+			auto: 'auto',
+			cover: 'cover',
+			contain: 'contain',
+			'50%': '50%',
+			'16': '4rem',
+			'4': '1rem',
+			'3': '0.75rem',
+			'2': '0.5rem'
+		},
 		extend: {
 			boxShadow: {
 				screen: '0 3px 10px rgba(255, 255, 255, 0.7)'
+			},
+			colors: {
+				'money-green': '#5fbaa7'
 			}
 		}
 	},
